b85f3f349a09c4c34d7e8b679511ae98de76792e
LampicJ15 <jan.lampic13@gmail.com>
2017-05-21 21:41:32 +0200

    Vizualizacija

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -10,22 +10,27 @@ prodaja <- read.csv2("podatki/urejeni/prodaja.csv")
 
 prodaja %>% group_by(Year) %>% summarise( Annual = sum(Number)) -> letna.prodaja
 
-graf1 <- ggplot(data = letna.prodaja) + aes(x=letna.prodaja$Year, y=letna.prodaja$Annual,fill=Year) + geom_bar(stat="identity")
+graf1 <- ggplot(data = letna.prodaja) + aes(x=letna.prodaja$Year, y=letna.prodaja$Annual) + geom_bar(stat="identity",fill ="cornflowerblue") + geom_line(color="red",size=1.2)
+graf1<- graf1 + xlab("Leto") + ylab("Število prodanih avtomobilov") +ggtitle("Letna prodaja avtomobilov")
 print(graf1)
-graf1 + xlab("Leto") + ylab("Število prodanih avtomobilov") +ggtitle("Letna prodaja avtomobilov")
-
 
 #uporaba po letih (ni loceno glede na tip vozila) (?? graf je treba popraviti x-os)
 uporaba <- read.csv2("podatki/urejeni/uporaba.csv")
 
 letna.uporaba  <- uporaba %>% group_by(Year) %>% summarise( Annual = sum(Number))
 
-graf2 <- ggplot(data = letna.uporaba) + aes(x=letna.uporaba$Year, y=letna.uporaba$Annual, fill=Year) + geom_bar(stat="identity") + scale_x_discrete() 
-graf2 + xlab("Leto") + ylab("Število prodanih avtomobilov") +ggtitle("Letna uporaba avtomobilov")
+graf2 <- ggplot(data = letna.uporaba) + aes(x=letna.uporaba$Year, y=letna.uporaba$Annual) + geom_bar(stat="identity",fill ="cornflowerblue") + scale_x_discrete() 
+graf2 <- graf2 + xlab("Leto") + ylab("Število prodanih avtomobilov") +ggtitle("Letna uporaba avtomobilov")
+print(graf2)
 
 #uporaba po letih loceno glede na tip vozila
 letna.uporaba2  <- uporaba %>% group_by(Year, Type) %>% summarise( Annual = sum(Number))
+PC <- letna.uporaba2 %>% filter(Type == "PC")
+CV <- letna.uporaba2 %>% filter(Type == "CV")
 
+graf3<- ggplot(data = NULL, aes(x = PC$Year)) + geom_bar(stat="identity", aes(x=PC$Year,y=PC$Annual),fill="cornflowerblue") + geom_bar(stat="identity", aes(x=PC$Year,y=CV$Annual),fill="red")
+graf3 <- graf3 + xlab("Leto") + ylab("Število prodanih avtomobilov") + ggtitle("Letna uporaba avtomobilov glede na tip") 
+print(graf3)
 
 #proizvodnja po letih
 proizvodnja <- read.csv2("podatki/urejeni/proizvodnja.csv")
