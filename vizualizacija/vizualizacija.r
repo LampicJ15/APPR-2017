a8e3fb140a53d79fcd34c2c3510f447b2ad75730
Jan Lampič <jan.lampic13@gmail.com>
2017-05-22 11:33:36 +0200

    Vizualizacija - dodajanje grafov

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -6,37 +6,56 @@ library(ggplot2)
 
 prodaja <- read.csv2("podatki/urejeni/prodaja.csv")
 
-#prodaja po letih 
+#prodaja po letih (ni ločeno glede na tip vozila)
 
-prodaja %>% group_by(Year) %>% summarise( Annual = sum(Number)) -> letna.prodaja
+letna.prodaja <- prodaja %>% group_by(Year) %>% summarise( Annual = sum(Number)) 
 
-graf1 <- ggplot(data = letna.prodaja) + aes(x=letna.prodaja$Year, y=letna.prodaja$Annual) + geom_bar(stat="identity",fill ="cornflowerblue") + geom_line(color="red",size=1.2)
-graf1<- graf1 + xlab("Leto") + ylab("Število prodanih avtomobilov") +ggtitle("Letna prodaja avtomobilov")
-print(graf1)
+graf.prodaja <- ggplot(data = letna.prodaja) + aes(x=Year, y=Annual) + geom_bar(stat="identity",fill ="cornflowerblue") + geom_line(color="red",size=1.2)
+graf.prodaja<- graf.prodaja + xlab("Leto") + ylab("Število prodanih avtomobilov") +ggtitle("Letna prodaja avtomobilov")
+print(graf.prodaja)
 
-#uporaba po letih (ni loceno glede na tip vozila) (?? graf je treba popraviti x-os)
+#prodaja po letih (ločeno glede na tip vozila)
+letna.prodaja2 <- prodaja %>% group_by(Year, Type) %>% summarise(Annual = sum(Number))
+graf.prodaja2 <- ggplot(data = letna.prodaja2, aes(x = factor(Year), y = Annual, fill = Type)) + geom_bar(stat="identity", position="dodge")
+graf.prodaja2 <- graf.prodaja2 + xlab("Leto") + ylab("Število prodanih avtomobilov") + ggtitle("Letna prodaja avtomobilov (ločeno)")
+print(graf.prodaja2)
+
+#uporaba po letih (ni loceno glede na tip vozila)
 uporaba <- read.csv2("podatki/urejeni/uporaba.csv")
 
 letna.uporaba  <- uporaba %>% group_by(Year) %>% summarise( Annual = sum(Number))
 
-graf2 <- ggplot(data = letna.uporaba) + aes(x=letna.uporaba$Year, y=letna.uporaba$Annual) + geom_bar(stat="identity",fill ="cornflowerblue") + scale_x_discrete() 
-graf2 <- graf2 + xlab("Leto") + ylab("Število prodanih avtomobilov") +ggtitle("Letna uporaba avtomobilov")
-print(graf2)
+graf.uporaba <- ggplot(data = letna.uporaba, aes(x=factor(Year), y=Annual)) + geom_bar(stat="identity",fill ="cornflowerblue")
+graf.uporaba <- graf.uporaba + xlab("Leto") + ylab("Število avtomobilov v uporabi") +ggtitle("Letna uporaba avtomobilov")
+print(graf.uporaba)
 
 #uporaba po letih loceno glede na tip vozila
 letna.uporaba2  <- uporaba %>% group_by(Year, Type) %>% summarise( Annual = sum(Number))
-PC <- letna.uporaba2 %>% filter(Type == "PC")
-CV <- letna.uporaba2 %>% filter(Type == "CV")
 
-graf3<- ggplot(data = NULL, aes(x = PC$Year)) + geom_bar(stat="identity", aes(x=PC$Year,y=PC$Annual),fill="cornflowerblue") + geom_bar(stat="identity", aes(x=PC$Year,y=CV$Annual),fill="red")
-graf3 <- graf3 + xlab("Leto") + ylab("Število prodanih avtomobilov") + ggtitle("Letna uporaba avtomobilov glede na tip") 
-print(graf3)
+graf.uporaba2<- ggplot(data = letna.uporaba2, aes(x = factor(Year), y = Annual, fill = Type)) + geom_bar(stat="identity", position="dodge")
+graf.uporaba2 <- graf.uporaba2 + xlab("Leto") + ylab("Število prodanih avtomobilov") + ggtitle("Letna uporaba avtomobilov glede na tip") 
+print(graf.uporaba2)
 
-#proizvodnja po letih
+#proizvodnja po letih (ni ločeno glede na tip vozila)
 proizvodnja <- read.csv2("podatki/urejeni/proizvodnja.csv")
+
+#proizvodnja po letih (ni ločeno glede na tip vozila)
+letna.proizvodnja <- proizvodnja %>% group_by(Year) %>% summarise(Annual = sum(Number, na.rm = TRUE)) #na.rm = TRUE mankajoče vrednosti odstranjene
+graf.proizvodnja <- ggplot(data = letna.proizvodnja, aes(x= factor(Year),y=Annual)) + geom_bar(stat="identity",fill="cornflowerblue")
+graf.proizvodnja <- graf.proizvodnja + xlab("Leto") +ylab("Število proizvedenih avtomobilov") + ggtitle("Letna proizvodnja avtomobilov")
+print(graf.proizvodnja)
+
+#proizvodnja po letih (ločeno glede na tip vozila)
+
+letna.proizvodnja2 <- proizvodnja %>% group_by(Year,Type) %>% summarise(Annual = sum(Number, na.rm = TRUE))
+graf.proizvodnja2 <- ggplot(data = letna.proizvodnja2, aes(x= factor(Year),y=Annual,fill=Type)) + geom_bar(stat="identity", position = "dodge")
+graf.proizvodnja2 <- graf.proizvodnja2 + xlab("Leto") +ylab("Število proizvedenih avtomobilov") + ggtitle("Letna proizvodnja avtomobilov")
+print(graf.proizvodnja2)
+
+
 #prikaz podatkov na zemljevidu za proizvodnjo v letu 2015
 
-pro2016 <- proizvodnja %>% filter(Year == 2015) %>%group_by(Country) %>% summarise(Production = sum(Number))
+pro2015 <- proizvodnja %>% filter(Year == 2015) %>%group_by(Country) %>% summarise(Production = sum(Number))
 
 
 #tabela geografske širine in višine posamezne države
@@ -47,7 +66,7 @@ koordinati$Country <- toupper(koordinati$Country)
 koordinati$Country <- parse_character(koordinati$Country)
 
 #dodamo podatke o koordinatah 
-pro2016 <- left_join(pro2016, koordinati, by="Country")
+pro2015 <- left_join(pro2015, koordinati, by="Country")
 
 #zemljevid sveta
 library(maps)
